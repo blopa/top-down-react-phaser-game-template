0525558d326e4e499b8cdada72c280f02c93b5df
Pablo Montenegro <montenegro.pablo@outlook.com>
2021-11-13 16:23:51 +0100

    add phaser packages

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,13 +1,59 @@
-import './Game.css';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Phaser from 'phaser';
+import GridEngine from 'grid-engine';
+
+// Styles
+import './Game.css';
+
+// Actions
 import { simpleAction } from './redux/actions/simpleAction';
 
 function Game() {
     const number = useSelector((state) => state.simple.number);
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        const game = new Phaser.Game({
+            type: Phaser.AUTO,
+            title: 'some-game-title',
+            parent: 'game-content',
+            orientation: Phaser.Scale.LANDSCAPE,
+            localStorageName: 'some-game-title',
+            // width,
+            // height,
+            autoRound: true,
+            pixelArt: true,
+            scale: {
+                autoCenter: Phaser.Scale.CENTER_BOTH,
+                mode: Phaser.Scale.ENVELOP,
+            },
+            scene: [],
+            physics: {
+                default: 'arcade',
+            },
+            plugins: {
+                scene: [
+                    {
+                        key: 'gridEngine',
+                        plugin: GridEngine,
+                        mapping: 'gridEngine',
+                    },
+                ],
+            },
+            backgroundColor: '#000000',
+        });
+
+        // window.phaserGame = game;
+    }, []);
+
     return (
         <div className="App">
+            <div
+                id="game-content"
+            >
+                {/* this is where the game canvas will be rendered */}
+            </div>
             {number}
             <button
                 type="button"
