ae5669259ce4516f0cb7454a0e914a1711e294f0
Pablo Montenegro <montenegro.pablo@outlook.com>
2022-04-23 15:37:38 +0200

    refactor message box

---
+++ src/components/MessageBox.jsx
@@ -0,0 +1,82 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+// Components
+import Message from './Message';
+
+// Selectors
+import { selectGameZoom } from '../redux/selectors/selectGameData';
+import { selectDialogAction, selectDialogCharacterName, selectDialogMessages } from '../redux/selectors/selectDialog';
+
+// Constants
+import { ENTER_KEY, ESCAPE_KEY, SPACE_KEY } from '../constants';
+
+const MessageBox = ({
+    showNext = false,
+    dialogWindowClassname = null,
+    dialogTitleClassname = null,
+    dialogFooterClassname = null,
+}) => {
+    const dialogMessages = useSelector(selectDialogMessages);
+    const dialogAction = useSelector(selectDialogAction);
+    const gameZoom = useSelector(selectGameZoom);
+    const characterName = useSelector(selectDialogCharacterName);
+
+    const [currentMessage, setCurrentMessage] = useState(0);
+    const [messageEnded, setMessageEnded] = useState(false);
+    const [forceShowFullMessage, setForceShowFullMessage] = useState(false);
+
+    const handleClick = useCallback(() => {
+        if (messageEnded) {
+            setMessageEnded(false);
+            setForceShowFullMessage(false);
+            if (currentMessage < dialogMessages.length - 1) {
+                setCurrentMessage(currentMessage + 1);
+            } else {
+                setCurrentMessage(0);
+                dialogAction?.();
+            }
+        } else {
+            setMessageEnded(true);
+            setForceShowFullMessage(true);
+        }
+    }, [dialogAction, currentMessage, messageEnded, dialogMessages.length]);
+
+    useEffect(() => {
+        const handleKeyPressed = (e) => {
+            if ([ENTER_KEY, SPACE_KEY, ESCAPE_KEY].includes(e.code)) {
+                handleClick();
+            }
+        };
+        window.addEventListener('keydown', handleKeyPressed);
+
+        return () => window.removeEventListener('keydown', handleKeyPressed);
+    }, [dialogMessages.length, handleClick]);
+
+    return (
+        <div className={dialogWindowClassname}>
+            <div className={dialogTitleClassname}>
+                {characterName}
+            </div>
+            <Message
+                message={dialogMessages[currentMessage]}
+                key={currentMessage}
+                zoom={gameZoom}
+                forceShowFullMessage={forceShowFullMessage}
+                onMessageEnded={() => {
+                    setMessageEnded(true);
+                }}
+            />
+            {showNext && (
+                <div
+                    onClick={handleClick}
+                    className={dialogFooterClassname}
+                >
+                    {(currentMessage === dialogMessages.length - 1 && messageEnded) ? 'Ok' : 'Next'}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default MessageBox;
